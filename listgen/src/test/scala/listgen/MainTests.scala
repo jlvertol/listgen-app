9bf64e99571bd9db0810697c3d3d72fad309baf1
José Luis Vergara <jlvertol@gmail.com>
2021-03-19 07:58:56 -0300

    Simple listgen backend. Assembly works.

---
+++ listgen/src/test/scala/listgen/MainTests.scala
@@ -0,0 +1,26 @@
+package listgen
+
+import org.apache.poi.ss.usermodel.Sheet
+import org.scalatest._
+import os.Path
+
+class MainTests extends FunSuite {
+  test("Saves file") {
+    val wd: Path = os.pwd
+    val filename = "sample list test file"
+    val path = (wd / "src" / "resources" / filename.+(".xlsx")).toString()
+    val sheet: Sheet = Main.openSheet(path, 1)
+    val list = Main.genList(sheet, 2, 4)
+    Main.saveTxt(path, filename, list)
+  }
+
+  test("Saves Smil") {
+    val wd: Path = os.pwd
+    val filename = "sample list test file"
+    val path = (wd / "src" / "resources" / filename.+(".xlsx")).toString()
+    val sheet: Sheet = Main.openSheet(path, 1)
+    val list = Main.genList(sheet, 2, 4)
+    val smil = Main.genSmil(list)
+    Main.saveSmil(path, filename, smil)
+  }
+}
